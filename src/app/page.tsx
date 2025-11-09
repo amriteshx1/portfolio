2e066f67f1c718dc3aff4fc853b972dd34e9f643
amriteshx1 <amriteshx18@gmail.com>
2025-11-09 21:57:05 +0530

    add introductory hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,20 @@ export default function Home() {
         </div>
       </div>
 
+      <div className="h-[20vh] w-full flex justify-center items-center border-b border-b-neutral-800">
+        <div className="h-full w-[60%] flex items-center justify-start border-x border-x-neutral-800">
+          <div className="h-full w-[20%] flex items-center justify-center border-r border-r-neutral-800 dotted-pattern">
+            {/* <img src="/me.png" alt="" className="max-h-full rounded-full bg-black border border-neutral-800" /> */}
+          </div>
+
+          <div className="h-full w-[80%] flex flex-col justify-center items-start text-stone-400">
+            <p className="ml-5 tracking-tight text-[15px]">Hey, I'm</p>
+            <p className="text-3xl font-medium tracking-tight text-white ml-5">Amritesh!</p>
+            <p className="ml-5 text-sm mt-3">&lt; Full-Stack Engineer, Systems Thinker, In-Code Designer, and a Student of the Craft. /&gt;</p>
+          </div>
+        </div>
+      </div>
+
       
     </div>
   );
